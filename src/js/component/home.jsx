05aa5b94b36b4245be23b0fdf4114db6d7a5f9d1
mayaharrison <jayaniharrison@gmail.com>
2021-12-01 01:40:48 +0000

    To do list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,41 @@
 import React from "react";
-
+import { useState } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [todo, setTodo] = useState([]);
+	const [value, setValue] = useState("");
+	const addTodo = onKeyDownEvent => {
+		if (onKeyDownEvent.keyCode === 13) {
+			const newTodo = [...todo, value];
+			setTodo(newTodo);
+			setValue("");
+		}
+	};
+	const items = todo.map((item, i) => {
+		return (
+			<li className="list-group-item item" key={i}>
+				{item}
+				<button onClick={() => deleteTodo(i)}>X</button>
+			</li>
+		);
+	});
+	const deleteTodo = index => {
+		const filterTodo = todo.filter((_, i) => i != index);
+		setTodo(filterTodo);
+	};
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<h1>ToDo List</h1>
+			<input
+				className="list-group-item input"
+				onKeyDown={addTodo}
+				onChange={e => setValue(e.target.value)}
+				value={value}></input>
+			<ul>{items}</ul>
+			<div>{items.length}</div>
 		</div>
 	);
 };
